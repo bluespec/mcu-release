5b76f768283417fde1c16bbe19123dee1f72ea32
Niraj N Sharma <niraj.sharma@bluespec.com>
2022-10-25 18:32:26 +0530

    Fixed Questasim makefile to not create objects inside install location

--- build/scripts/makefiles/Include_questasim.mk
+++ build/scripts/makefiles/Include_questasim.mk
@@ -13,21 +13,21 @@ QUESTA_RESOURCES = $(SCRIPTS_DIR)/makefiles/Questasim_resources
 
 .PHONY: simulator
 simulator:
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/lib/C/C_Imported_Functions.o" "$(QUESTA_RESOURCES)/lib/C/C_Imported_Functions.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_end_timing.o"     "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_end_timing.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/lib/C/sim_socket.o" "$(QUESTA_RESOURCES)/lib/C/sim_socket.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/lib/C/sim_dmi.o" "$(QUESTA_RESOURCES)/lib/C/sim_dmi.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_end_timing.o"     "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_end_timing.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_get_symbol_val.o" "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_get_symbol_val.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_start_timing.o"   "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_start_timing.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_trygetchar.o"     "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_trygetchar.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_open.o"            "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_open.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_accept.o"            "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_accept.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_getchar.o"            "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_getchar.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_putchar.o"            "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_putchar.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_vpidmi_request.o"            "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_vpidmi_request.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_vpidmi_response.o"            "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_vpidmi_response.c"
-	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(QUESTA_RESOURCES)/C_VPI/vpi_startup_array.o"            "$(QUESTA_RESOURCES)/C_VPI/vpi_startup_array.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64                                 -fpermissive -fPIC -c -o "$(SIM_RTL)/C_Imported_Functions.o" 		"$(QUESTA_RESOURCES)/lib/C/C_Imported_Functions.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(SIM_RTL)/vpi_wrapper_c_end_timing.o"     	"$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_end_timing.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64                                 -fpermissive -fPIC -c -o "$(SIM_RTL)/sim_socket.o" 			"$(QUESTA_RESOURCES)/lib/C/sim_socket.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64                                 -fpermissive -fPIC -c -o "$(SIM_RTL)/sim_dmi.o" 			"$(QUESTA_RESOURCES)/lib/C/sim_dmi.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(SIM_RTL)/vpi_wrapper_c_end_timing.o"       "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_end_timing.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(SIM_RTL)/vpi_wrapper_c_get_symbol_val.o"   "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_get_symbol_val.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(SIM_RTL)/vpi_wrapper_c_start_timing.o"     "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_start_timing.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(SIM_RTL)/vpi_wrapper_c_trygetchar.o"       "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_trygetchar.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(SIM_RTL)/vpi_wrapper_sim_socket_open.o"    "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_open.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(SIM_RTL)/vpi_wrapper_sim_socket_accept.o"  "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_accept.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(SIM_RTL)/vpi_wrapper_sim_socket_getchar.o" "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_getchar.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(SIM_RTL)/vpi_wrapper_sim_socket_putchar.o" "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_putchar.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(SIM_RTL)/vpi_wrapper_vpidmi_request.o"     "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_vpidmi_request.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(SIM_RTL)/vpi_wrapper_vpidmi_response.o"    "$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_vpidmi_response.c"
+	cc -O3  -Wall -Wno-unused -D_FILE_OFFSET_BITS=64 -I"$(QUESTA_RESOURCES)/lib"/VPI -fpermissive -fPIC -c -o "$(SIM_RTL)/vpi_startup_array.o"              "$(QUESTA_RESOURCES)/C_VPI/vpi_startup_array.c"
 
 #compile Questa simulation
 	$(SCRIPTS_DIR)/makefiles/bsc_build_vsim_modelsim link $(SIM_EXE_FILE) mkTop_HW_Side \
@@ -35,20 +35,20 @@ simulator:
 	-y $(USER_RTL_DIR) \
 	-y $(SIM_RTL) \
 	-y $(QUESTA_RESOURCES)/lib/Verilog \	$(QUESTA_RESOURCES)/lib/Verilog/main.v \
-	$(QUESTA_RESOURCES)/lib/C/sim_socket.o \
-	$(QUESTA_RESOURCES)/lib/C/C_Imported_Functions.o \
-	$(QUESTA_RESOURCES)/lib/C/sim_dmi.o \
-	$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_open.o \
-	$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_accept.o \
-	$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_putchar.o \
-	$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_getchar.o \
-	$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_end_timing.o \
-	$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_get_symbol_val.o \
-	$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_start_timing.o \
-	$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_trygetchar.o \
-	$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_vpidmi_response.o \
-	$(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_vpidmi_request.o \
-	$(QUESTA_RESOURCES)/C_VPI/vpi_startup_array.o
+	$(SIM_RTL)/sim_socket.o \
+	$(SIM_RTL)/C_Imported_Functions.o \
+	$(SIM_RTL)/sim_dmi.o \
+	$(SIM_RTL)/vpi_wrapper_sim_socket_open.o \
+	$(SIM_RTL)/vpi_wrapper_sim_socket_accept.o \
+	$(SIM_RTL)/vpi_wrapper_sim_socket_putchar.o \
+	$(SIM_RTL)/vpi_wrapper_sim_socket_getchar.o \
+	$(SIM_RTL)/vpi_wrapper_c_end_timing.o \
+	$(SIM_RTL)/vpi_wrapper_c_get_symbol_val.o \
+	$(SIM_RTL)/vpi_wrapper_c_start_timing.o \
+	$(SIM_RTL)/vpi_wrapper_c_trygetchar.o \
+	$(SIM_RTL)/vpi_wrapper_vpidmi_response.o \
+	$(SIM_RTL)/vpi_wrapper_vpidmi_request.o \
+	$(SIM_RTL)/vpi_startup_array.o
 
 
 	vlib work_mkTop_HW_Side
@@ -58,17 +58,17 @@ simulator:
 	    -o directc_mkTop_HW_Side.so \
 	    -Wl,-rpath,$(QUESTA_RESOURCES)/lib/VPI/g++4_64 \
 	    -L $(QUESTA_RESOURCES)/lib/VPI/g++4_64  \
-	    $(QUESTA_RESOURCES)/lib/C/C_Imported_Functions.o \
-	    $(QUESTA_RESOURCES)/lib/C/sim_socket.o \
-	    $(QUESTA_RESOURCES)/lib/C/sim_dmi.o \
-	    $(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_end_timing.o \
-	    $(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_get_symbol_val.o \
-	    $(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_start_timing.o \
-	    $(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_c_trygetchar.o \
-	    $(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_open.o \
-	    $(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_accept.o \
-	    $(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_getchar.o \
-	    $(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_sim_socket_putchar.o \
-	    $(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_vpidmi_request.o \
-	    $(QUESTA_RESOURCES)/C_VPI/vpi_wrapper_vpidmi_response.o \
-	    $(QUESTA_RESOURCES)/C_VPI/vpi_startup_array.o  -lbdpi
+	    $(SIM_RTL)/C_Imported_Functions.o \
+	    $(SIM_RTL)/sim_socket.o \
+	    $(SIM_RTL)/sim_dmi.o \
+	    $(SIM_RTL)/vpi_wrapper_c_end_timing.o \
+	    $(SIM_RTL)/vpi_wrapper_c_get_symbol_val.o \
+	    $(SIM_RTL)/vpi_wrapper_c_start_timing.o \
+	    $(SIM_RTL)/vpi_wrapper_c_trygetchar.o \
+	    $(SIM_RTL)/vpi_wrapper_sim_socket_open.o \
+	    $(SIM_RTL)/vpi_wrapper_sim_socket_accept.o \
+	    $(SIM_RTL)/vpi_wrapper_sim_socket_getchar.o \
+	    $(SIM_RTL)/vpi_wrapper_sim_socket_putchar.o \
+	    $(SIM_RTL)/vpi_wrapper_vpidmi_request.o \
+	    $(SIM_RTL)/vpi_wrapper_vpidmi_response.o \
+	    $(SIM_RTL)/vpi_startup_array.o 
